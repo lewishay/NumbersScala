a65abb5dd031da5a85ac1744ad370ad2ecd94a6b
lewishayqa <lewis.hay@qa.com>
2017-07-18 17:16:41 +0000

    Small changes to code style.

--- src/test/scala/NumSpec.scala
+++ src/test/scala/NumSpec.scala
@@ -1,6 +1,7 @@
-import org.scalatest.{FlatSpec, Matchers}
-
 /**
   * Created by Administrator on 18/07/2017.
   */
+
+import org.scalatest.{FlatSpec, Matchers}
+
 abstract class NumSpec extends FlatSpec with Matchers
